0b457847c7febfe284a2a73f702c5aafb6c9e275
RS <132844576+RuimingShen@users.noreply.github.com>
2025-10-02 22:27:43 -0700

    Create index.tsx

---
+++ src/index.tsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import { createRoot } from 'react-dom/client';
+import { HashRouter } from 'react-router-dom';
+import App from './App';
+import './index.css';
+
+
+const container = document.getElementById('root');
+if (!container) throw new Error('#root not found');
+
+
+createRoot(container).render(
+<React.StrictMode>
+<HashRouter>
+<App />
+</HashRouter>
+</React.StrictMode>
+);
